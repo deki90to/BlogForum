5ad64453b1a193dc019145ebb81666a9c1eb7a38
deki90to <deki90to@gmail.com>
2020-11-14 18:11:40 +0100

    fixed: authentication, automat.pick post-author, login-regis form

--- test_app/forms.py
+++ test_app/forms.py
@@ -6,3 +6,8 @@ class PostForm(forms.ModelForm):
 	class Meta:
 		model = Post
 		fields = ('author', 'body',)
+
+		widgets = {
+			'author':forms.TextInput(attrs={'class': 'form-control', 'value':'', 'id':'admin', 'type':'hidden'}),
+			'body':forms.Textarea(attrs={'class': 'form-control', 'placeholder':'Type comment...', 'rows':3, 'cols':10}),
+		}
